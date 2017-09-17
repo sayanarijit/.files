6dba3822d5ec915e44a9ec19fbad5c4d1ac108b4
Arijit Basu <sayanarijit@users.noreply.github.com>
2017-09-17 23:18:45 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,33 @@
+<div class="right_col" role="main">
+
+  <!-- Admin controls -->
+  <div class="row login_wrapper">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <div class="form">
+        <section class="login_content col-md-4 col-sm-6 col-xs-6">
+          <form method="post">
+            <h1>Admin login</h1>
+            <div>
+              <input name="username" type="text" class="form-control" placeholder="Username" required>
+            </div>
+            <div>
+              <input name="password" type="password" class="form-control" placeholder="Password" required>
+            </div>
+            <div>
+              <button name="login" class="btn btn-default btn-block submit" type="submit" >Log in</button>
+            </div>
+
+            <div class="clearfix"></div>
+          </form>
+        </section>
+      </div>
+      <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+      <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+      <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+      <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+      <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+      <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+      <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+    </div>
+  </div>
+</div>
