90908d857fa2e71431434151698fc09c81605ed4
Arijit Basu <sayanarijit@users.noreply.github.com>
2017-09-23 11:22:33 +0530

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -3,19 +3,19 @@
 # Kill process accessing nfs vols with fuser
 mount|grep nfs|cut -d" " -f1|while read -r line; do fuser -k $line & done
 
-# If fuser fails
+# If fuser fails, kill with lsof
 mount|grep nfs|cut -d" " -f1|while read -r line; do lsof $line|awk '{print $2}'|while read -r pid; do kill -9 $pid & done; done
 
 # Wait 10 sec
 sleep 10
 
-# Try forces unmount
+# Try forced unmount
 mount|grep nfs|cut -d" " -f1|while read -r line; do umount -fv $line & done
 
 # Wait for 10 sec
 sleep 10
 
-# Start lazy unmount on remaining mounts
+# Run lazy unmount on remaining mounts
 mount|grep nfs|cut -d" " -f1|while read -r line; do umount -lfv $line & done
 
 # Wait 5 sec
