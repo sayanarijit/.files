4353b4f1cb7a363694a9fdb4045d86d619c9ca82
Arijit Basu <sayanarijit@gmail.com>
2020-09-11 17:45:28 +0530

    Fri Sep 11 17:45:24 IST 2020

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -60,7 +60,7 @@ in
 
   # You should generally set this to the total number of logical cores in your system.
   # $ sysctl -n hw.ncpu
-  nix.maxJobs = 1;
+  nix.maxJobs = 8;
   nix.buildCores = 1;
   nix.useDaemon = false;
   nix.useSandbox = false;
