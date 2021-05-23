0ab0f045e5e25e0144f330e8a259145f95b25218
Arijit Basu <sayanarijit@gmail.com>
2021-05-23 10:24:13 +0530

    Sun May 23 10:24:10 AM IST 2021

--- nixpkgs/files/xplr/init.lua
+++ nixpkgs/files/xplr/init.lua
@@ -1,5 +1,4 @@
-version = "0.10.0-beta.5"
-
+version = "0.10.0"
 
 xplr.config.general.default_ui.prefix = " "
 xplr.config.general.default_ui.suffix = ""
@@ -16,6 +15,104 @@ xplr.config.general.panel_ui.default.style.bg = { Rgb = { 33, 33, 33 } }
 
 xplr.config.general.panel_ui.default.style.fg = { Rgb = { 170, 150, 130 } }
 
-xplr.config.node_types.directory.meta.icon = ""
-xplr.config.node_types.file.meta.icon = ""
-xplr.config.node_types.symlink.meta.icon = ""
+xplr.config.node_types.directory = {
+  style = {
+    fg = "Cyan",
+  },
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.node_types.file = {
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.node_types.symlink = {
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.node_types.mime_essence["text/plain"] = {
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.node_types.mime_essence["text/csv"] = {
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.node_types.extension["md"] = {
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.node_types.extension["lock"] = {
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.node_types.extension["rs"] = {
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.node_types.special["Cargo.toml"] = {
+  meta = {
+    icon = "",
+  },
+}
+
+xplr.config.modes.builtin.default.key_bindings.on_key["F"] = {
+  help = "fzf mode",
+  messages = {
+    {
+      SwitchModeCustom = "fzxplr",
+    },
+    "Refresh",
+  },
+}
+
+xplr.config.modes.custom["fzxplr"] = {
+  name = "fzxplr",
+  key_bindings = {
+    remaps = {
+      ["/"] = "F",
+    },
+    on_key = {
+      F = {
+        help = "search",
+        messages = {
+          {
+            BashExec = [===[
+            PTH=$(cat "${XPLR_PIPE_DIRECTORY_NODES_OUT:?}" | awk -F/ '{print $NF}' | fzf)
+            if [ -d "$PTH" ]; then
+              echo ChangeDirectory: "'"${PWD:?}/${PTH:?}"'" >> "${XPLR_PIPE_MSG_IN:?}"
+            elif [ -f "$PTH" ]; then
+              echo FocusPath: "'"${PWD:?}/${PTH:?}"'" >> "${XPLR_PIPE_MSG_IN:?}"
+            fi
+            ]===]
+          },
+          "PopMode",
+          "Refresh",
+        },
+      },
+    },
+    default = {
+      messages = {
+        "PopMode",
+        "Refresh",
+      },
+    },
+  },
+}
+
