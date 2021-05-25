41be14794568326ae79266da3ba0014dfcfd4105
Arijit Basu <sayanarijit@gmail.com>
2021-05-25 17:57:13 +0530

    Tue May 25 05:57:10 PM IST 2021

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -59,7 +59,7 @@ in
       (
         import (
           builtins.fetchTarball {
-            url = https://github.com/nix-community/neovim-nightly-overlay/archive/2b2b5cb6eda3e2d8dd369655694e42378cf50e1e.tar.gz;
+            url = https://github.com/nix-community/neovim-nightly-overlay/archive/125b7af69ec99e79749877cd820d614f35a64a29.tar.gz;
           }
         )
       )
