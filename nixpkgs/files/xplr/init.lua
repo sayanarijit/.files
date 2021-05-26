d101c9e3e84dd90dc6297e56d2f0d860637e2d71
Arijit Basu <sayanarijit@gmail.com>
2021-05-26 07:35:57 +0530

    Wed May 26 07:35:53 AM IST 2021

--- nixpkgs/files/xplr/init.lua
+++ nixpkgs/files/xplr/init.lua
@@ -3,3 +3,11 @@ version = "0.12.0"
 package.path = os.getenv("HOME") .. '/.config/xplr/?/init.lua'
 
 require("icons").setup{}
+
+xplr.config.general.table.col_widths = {
+    { Length = 7 },
+    { Percentage = 50 },
+    { Percentage = 10 },
+    { Percentage = 10 },
+    { Min = 1 },
+}
