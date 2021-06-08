d0e76c106e7cd62217185c5a112477854982c623
Arijit Basu <sayanarijit@gmail.com>
2021-06-08 23:31:49 +0530

    Tue Jun  8 11:31:46 PM IST 2021

--- nixpkgs/files/xplr/init.lua
+++ nixpkgs/files/xplr/init.lua
@@ -22,3 +22,11 @@ require("nnn_preview_wrapper").setup{
 
 xplr.config.general.enable_mouse = true
 xplr.config.general.disable_recover_mode = false
+
+xplr.config.modes.builtin.default.key_bindings.on_key.x = {
+    messages = {
+        {
+            FocusPath = "/home/sayanarijit/Documents/GitHub/xplr",
+        },
+    },
+}
