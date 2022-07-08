8ec6ef88c97fd6f02d24cea9f7a89cdf41fe469f
Arijit Basu <sayanarijit@gmail.com>
2022-07-08 16:44:13 +0530

    Fri Jul  8 04:44:10 PM IST 2022

--- nixpkgs/files/nvim/lua/plugins.lua
+++ nixpkgs/files/nvim/lua/plugins.lua
@@ -378,21 +378,22 @@ require("packer").startup(function(use)
             end
           end, { "i", "s" }),
 
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          -- ["<Tab>"] = cmp.mapping(function(fallback)
+          --   if luasnip.jumpable() then
+          --     luasnip.expand_or_jump()
+          --   else
+          --     fallback()
+          --   end
+          -- end, { "i", "s" }),
+          ["<Tab>"] = { i = cmp.config.disable, c = cmp.config.disable },
 
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+          --   if luasnip.jumpable(-1) then
+          --     luasnip.jump(-1)
+          --   else
+          --     fallback()
+          --   end
+          -- end, { "i", "s" }),
         },
 
         sources = {
