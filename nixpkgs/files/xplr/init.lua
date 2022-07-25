36c811504a46d3f3347ddbfe2a9b040ba4bc02bd
Arijit Basu <sayanarijit@gmail.com>
2022-07-25 22:37:32 +0530

    Mon Jul 25 10:37:29 PM IST 2022

--- nixpkgs/files/xplr/init.lua
+++ nixpkgs/files/xplr/init.lua
@@ -274,6 +274,78 @@ xplr.config.modes.builtin.action.key_bindings.on_key.P = {
   },
 }
 
+local function stat(node)
+  return node.mime_essence
+end
+
+local function read(path, lines)
+  local out = ""
+  local p = io.open(path)
+
+  if p == nil then
+    return stat(path)
+  end
+
+  local i = 0
+  for line in p:lines() do
+    out = out .. line .. "\n"
+    if i == lines then
+      break
+    end
+    i = i + 1
+  end
+  p:close()
+
+  return out
+end
+
+xplr.config.layouts.custom.preview_pane = {
+  Horizontal = {
+    config = {
+      constraints = {
+        { Percentage = 60 },
+        { Percentage = 40 },
+      },
+    },
+    splits = {
+      "Table",
+      {
+        CustomContent = {
+          title = "preview",
+          body = { DynamicParagraph = { render = "custom.preview_pane.render" } },
+        },
+      },
+    },
+  },
+}
+
+xplr.fn.custom.preview_pane = {}
+xplr.fn.custom.preview_pane.render = function(ctx)
+  local n = ctx.app.focused_node
+
+  if n and n.canonical then
+    n = n.canonical
+  end
+
+  if n then
+    if n.is_file then
+      return read(n.absolute_path, ctx.layout_size.height)
+    else
+      return stat(n)
+    end
+  else
+    return ""
+  end
+end
+
+xplr.config.modes.builtin.switch_layout.key_bindings.on_key.p = {
+  help = "preview pane",
+  messages = {
+    "PopMode",
+    { SwitchLayoutCustom = "preview_pane" },
+  },
+}
+
 -- Fennel Support
 local fennel = require("fennel")
 
