becb9af22525683574a5b5d2f6354b7b4b9f7969
Arijit Basu <sayanarijit@gmail.com>
2023-06-18 18:53:07 +0530

    Compact

--- nixos/rpi.nix
+++ nixos/rpi.nix
@@ -2,22 +2,10 @@ let
   base = (import ./home.nix { inherit config pkgs; });
 in
 
-{
-  # ~/.config/nixpkgs/config.nix
-  nixpkgs = {
-    allowUnfree = true;
-    allowUnsupportedSystem = true;
-  };
-
-  # ~/.config/nixpkgs/home.nix
-  home = base.home // {
-    stateVersion = "23.05";
-    packages = with pkgs; [
-      vivid
-      luarocks
-    ];
-  };
-
-  # ~/.config/nixpkgs/home.nix
-  programs = base.programs;
+base // {
+  home.stateVersion = "23.05";
+  home.packages = with pkgs; [
+    vivid
+    luarocks
+  ];
 }
