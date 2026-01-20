d62a0f74e08bd78171e320c25e33172b2662014f
Arijit Basu <sayanarijit@gmail.com>
2026-01-20 12:31:19 +0530

    Tue Jan 20 12:31:15 PM IST 2026

---
+++ nixos/katana/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      katana = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+	specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
